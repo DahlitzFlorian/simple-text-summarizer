05512089f9c5059f3d7e6fe18ece747ac8070631
Florian Dahlitz <florian.dahlitz@ibm.com>
2018-12-27 18:53:44 +0100

    feat: added cli arguments

---
+++ text-summarizer.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def main():
+    args = parse_arguments()
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', help='File name of text to summarize')
+    parser.add_argument('-l', '--length', default=4, help='Number of sentences to return')
+    args = parser.parse_args()
+
+    return args
